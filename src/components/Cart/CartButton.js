ed7b90949c1e41575b5956f501bf222eeff35de6
shimonshachar <simboy2000@gmail.com>
2022-10-03 21:49:08 +0300

    redux cart-slice and ui-slice

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
+	const cartQuantity = useSelector(state => state.cart.totalQuantity);
+	const dispatch = useDispatch();
+	
+	const toggleHandler = () => {
+		dispatch(uiActions.toggle());
+	}
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
